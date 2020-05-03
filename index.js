3a5868236b5eeb21b1b9a57556420448efd998a7
Rvindjitta <rvindjitta11@gmail.com>
2020-05-03 21:32:32 +0530

    navigation

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
- import { AppRegistry, Platform } from 'react-native';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent('ThirdApp', () => App);
-
-if (Platform.OS === 'web') {
-  const rootTag = document.getElementById('root') || document.getElementById('main');
-  AppRegistry.runApplication('ThirdApp', { rootTag });
-}
-
-
-
-
-
+AppRegistry.registerComponent(appName, () => App);
